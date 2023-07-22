9f69ed3c727c6c3c11c8356d20064aeb626ec072
ignaz chou <zhpjie@gmail.com>
2023-07-22 23:18:26 +0800

    添加注释

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,26 +1,32 @@
+// 导入所需的模块
 import { useSelector } from 'react-redux';
 
+// 导入 Material-UI 组件
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
 
-// routing
+// 导入路由模块
 import Routes from 'routes';
 
-// defaultTheme
+// 导入默认主题
 import themes from 'themes';
 
-// project imports
+// 导入项目内部模块
 import NavigationScroll from 'layout/NavigationScroll';
 
 // ==============================|| APP ||============================== //
 
 const App = () => {
+  // 使用 useSelector 钩子来获取 Redux 中 customization 的状态
   const customization = useSelector((state) => state.customization);
 
   return (
     <StyledEngineProvider injectFirst>
+      {/* 使用 ThemeProvider 来设置 Material-UI 主题 */}
       <ThemeProvider theme={themes(customization)}>
+        {/* 使用 CssBaseline 来重置全局样式 */}
         <CssBaseline />
+        {/* 使用 NavigationScroll 组件包裹 Routes 组件，用于处理导航滚动效果 */}
         <NavigationScroll>
           <Routes />
         </NavigationScroll>
--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,15 +1,16 @@
+// 导入所需的模块
 import { createRoot } from 'react-dom/client';
 
-// third party
+// 导入第三方库
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-// project imports
+// 导入项目内部模块
 import * as serviceWorker from 'serviceWorker';
 import App from 'App';
 import { store } from 'store';
 
-// style + assets
+// 导入样式和资源
 import 'assets/scss/style.scss';
 import config from './config';
 
@@ -17,15 +18,17 @@ import config from './config';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+// 使用React的根渲染函数createRoot来将组件渲染到页面上
 root.render(
   <Provider store={store}>
+    {/* 使用BrowserRouter组件来启用基于浏览器的路由 */}
     <BrowserRouter basename={config.basename}>
       <App />
     </BrowserRouter>
   </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// 如果你希望你的应用程序能够离线运行并更快地加载，你可以将unregister()改为register()
+// 了解有关service workers的更多信息：https://bit.ly/CRA-PWA
 serviceWorker.unregister();
